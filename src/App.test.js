c09d16b72b8f335f1e8c63195bbd633990fc8d9b
Carl Beckley <carlbeckley@gmail.com>
2023-01-22 00:58:11 +0000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders weather app text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/weather app/i);
   expect(linkElement).toBeInTheDocument();
 });
