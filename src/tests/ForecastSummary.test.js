44e1f30cc5297e0b4ae6ab6e9a9e17376b5cffcf
Carl Beckley <carlbeckley@gmail.com>
2023-01-25 15:37:31 +0000

    updated tests to test props are rendered

--- src/tests/ForecastSummary.test.js
+++ src/tests/ForecastSummary.test.js
@@ -24,4 +24,21 @@ describe("ForecastSummary", () => {
     );
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it("renders the correct values for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    expect(getByText("12345")).toHaveClass("forecast-summary_date");
+    expect(getByText("Stub description")).toHaveClass(
+      "forecast-summary_description"
+    );
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary_icon");
+    expect(getByText("22°C")).toHaveClass("forecast-summary_temperature");
+  });
 });
