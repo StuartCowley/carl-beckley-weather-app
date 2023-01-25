7e556be3898a3928a4c8e6b03031602e7dcad863
Carl Beckley <carlbeckley@gmail.com>
2023-01-25 19:41:20 +0000

    added stylesheets and tweaked initial layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import "../styles/App.css";
 
 function App({ location, forecasts }) {
   return (
-    <div className="forecast">
+    <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
 
       <ForecastSummaries forecasts={forecasts} />
