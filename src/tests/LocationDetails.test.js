86adb3881d5066adcc56160128e84729fafc83a5
Carl Beckley <carlbeckley@gmail.com>
2023-01-25 11:59:27 +0000

    updated tests to include testing for heading element

--- src/tests/LocationDetails.test.js
+++ src/tests/LocationDetails.test.js
@@ -9,4 +9,11 @@ describe("LocationDetails", () => {
     );
     expect(getByText("Manchester, UK")).toBeTruthy();
   });
+  it("renders the correct city and location inside the heading element", () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+
+    expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+  });
 });
