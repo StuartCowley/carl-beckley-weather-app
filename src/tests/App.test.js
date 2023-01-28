cea55496db6bf7a94f4d9c5140f8066df2f24232
Carl Beckley <carlbeckley@gmail.com>
2023-01-28 14:52:13 +0000

    added missing forecasts prop to fix test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,37 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "../components/App";
-import forecast from "../data/forecast.json";
+// import forecast from "../data/forecast.json";
 
 describe("App", () => {
+  const validProps = {
+    location: {
+      city: "test city",
+      country: "test country",
+    },
+    forecasts: [
+      {
+        date: 1111111,
+        description: "Stub description",
+        icon: "800",
+        temperature: {
+          min: 12,
+          max: 22,
+        },
+        humidity: 20,
+        wind: {
+          direction: "direction",
+          speed: 20,
+        },
+      },
+    ],
+  };
+
   test("renders App component correctly", () => {
-    render(<App location={forecast.location} />);
-    const h1Element = screen.getByText(/Manchester, UK/i);
+    render(
+      <App location={validProps.location} forecasts={validProps.forecasts} />
+    );
+    const h1Element = screen.getByText(/test city, test country/i);
     expect(h1Element).toBeInTheDocument();
   });
 });
