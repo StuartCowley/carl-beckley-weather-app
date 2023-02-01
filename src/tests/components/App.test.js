67a0931991f365740ad338329473b8a263a29f81
Carl Beckley <carlbeckley@gmail.com>
2023-02-01 15:19:08 +0000

    removed validProps and changed test to check snapshot as App no longer receives props

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,37 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import React from "react";
 import App from "../../components/App";
-// import forecast from "../data/forecast.json";
 
 describe("App", () => {
-  const validProps = {
-    location: {
-      city: "test city",
-      country: "test country",
-    },
-    forecasts: [
-      {
-        date: 1111111,
-        description: "Stub description",
-        icon: "800",
-        temperature: {
-          min: 12,
-          max: 22,
-        },
-        humidity: 20,
-        wind: {
-          direction: "direction",
-          speed: 20,
-        },
-      },
-    ],
-  };
-
   test("renders App component correctly", () => {
-    render(
-      <App location={validProps.location} forecasts={validProps.forecasts} />
-    );
-    const h1Element = screen.getByText(/test city, test country/i);
-    expect(h1Element).toBeInTheDocument();
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
