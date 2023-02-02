606d7262d34f918a4ef7549c136b5572a3982020
Carl Beckley <carlbeckley@gmail.com>
2023-02-02 15:10:24 +0000

    added error message for 404 and 500 status codes

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import axios from "axios";
 
 const getForecast = (
@@ -12,11 +13,22 @@ const getForecast = (
     endpoint += `?city=${searchText}`;
   }
 
-  return axios.get(endpoint).then((response) => {
-    setSelectedDate(response.data.forecasts[0].date);
-    setForecasts(response.data.forecasts);
-    setLocation(response.data.location);
-  });
+  return axios
+    .get(endpoint)
+    .then((response) => {
+      setSelectedDate(response.data.forecasts[0].date);
+      setForecasts(response.data.forecasts);
+      setLocation(response.data.location);
+    })
+    .catch((error) => {
+      const { status } = error.response;
+      if (status === 404) {
+        console.error("Location is not valid", error);
+      }
+      if (status === 500) {
+        console.error("Server error", error);
+      }
+    });
 };
 
 export default getForecast;
